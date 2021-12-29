e7bfda3f5ba5d742e795063eff8943ddebc62427
ruben.sabido <ruben.sabido@stalicla.com>
2021-12-29 16:16:16 +0100

    update

--- generar.py
+++ generar.py
@@ -12,6 +12,17 @@ from io import BytesIO
 def all_same(items):
     return all(x == items[0] for x in items)
 
+def ImageURL(url, size_x, size_y):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    newsize=(size_x,size_y)
+    return img.resize(newsize)
+
+def ImageLocal(directory, size_x, size_y):
+    img = Image.open(directory)
+    newsize=(size_x,size_y)
+    return img.resize(newsize)
+
 #Types dictionary
 types = {
     1: {'name': 'normal', 'sprite': 'https://images.wikidexcdn.net/mwuploads/wikidex/c/c4/latest/20191221233818/Tipo_normal.png', 'color': '#A8A878', 'color_dark': '#6D6D4E', 'color_light': '#C6C6A7'},
@@ -58,32 +69,22 @@ pokemon_forms.loc[pokemon_forms["form_identifier"] == "mega-y", "form_identifier
 pokemon_forms.loc[pokemon_forms["form_identifier"] == "eternamax", "form_identifier"] = "gmax"
 
 #Define fonts
-font_name = ImageFont.truetype("/Users/ruben.sabido/Downloads/Flexo-Medium.ttf", 32)
-font_desc = ImageFont.truetype("/Users/ruben.sabido/Downloads/Flexo-Medium.ttf", 20)
-font_num = ImageFont.truetype("/Users/ruben.sabido/Downloads/Meslo_LG_S_Regular_400.ttf", 36)
-font_data = ImageFont.truetype("/Users/ruben.sabido/Downloads/Flexo-Medium.ttf", 18)
-font_data2 = ImageFont.truetype("/Users/ruben.sabido/Downloads/Flexo-Bold.ttf", 18)
+font_name = ImageFont.truetype("resources/fonts/Flexo-Medium.ttf", 32)
+font_desc = ImageFont.truetype("resources/fonts/Flexo-Medium.ttf", 20)
+font_num = ImageFont.truetype("resources/fonts/Meslo_LG_S_Regular_400.ttf", 36)
+font_data = ImageFont.truetype("resources/fonts/Flexo-Medium.ttf", 18)
+font_data2 = ImageFont.truetype("resources/fonts/Flexo-Bold.ttf", 18)
 
 #Generate individual figures for default Pokemons
 
-for mon in range(1,899):
-    url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+str(mon)+".png"
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
-    newsize=(400,400)
-    img =img.resize(newsize)
+for mon in range(300,899):
+    img =ImageURL("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+str(mon)+".png",400,400)
     n_types=len(pokemon_types[pokemon_types["pokemon_id"] == mon])
     type1_pkmn=pokemon_types[(pokemon_types["slot"] == 1) & (pokemon_types["pokemon_id"] == mon)].type_id.values.astype(int)[0]
-    response2 = requests.get(types[type1_pkmn]['sprite'])
-    img2 = Image.open(BytesIO(response2.content))
-    newsize=(100,22)
-    img2 = img2.resize(newsize)
+    img2=ImageURL(types[type1_pkmn]['sprite'],100,22)
     if n_types == 2:
         type2_pkmn=pokemon_types[(pokemon_types["slot"] == 2) & (pokemon_types["pokemon_id"] == mon)].type_id.values.astype(int)[0]
-        response3 = requests.get(types[type2_pkmn]['sprite'])
-        img3 = Image.open(BytesIO(response3.content))
-        newsize=(100,22)
-        img3=img3.resize(newsize)
+        img3=ImageURL(types[type2_pkmn]['sprite'],100,22)
     with Image.new('RGBA', (400, 580)) as im:
         im.paste(img, (0, 100))
         if n_types == 1:
@@ -103,16 +104,13 @@ for mon in range(1,899):
         draw.text((210,548), "Peso:", fill = "black", font = font_data)
         draw.text((123,548), str(round(pokemon[(pokemon["id"] == mon)].height.values[0]*0.1,1))+' m', fill = "black", font = font_data2)
         draw.text((260,548), str(round(pokemon[(pokemon["id"] == mon)].weight.values[0]*0.1,1))+' kg', fill = "black", font = font_data2)
-    im.save("figures/"+str(mon)+".png")
-
-
-
+    im.save("figures/pokemon_"+str(mon)+".png")
 
 
 
 
 #Evo chains
-test = pokemon_species[pokemon_species["evolution_chain_id"] == 135]
+test = pokemon_species[pokemon_species["evolution_chain_id"] == 26]
 
 #Pokemons in the evolutionay chain
 nrow_evo = test.evolves_from_species_id.value_counts().max()
@@ -130,7 +128,8 @@ if len(matrix[0]) > 1:
         while matrix[x,y] in test.evolves_from_species_id.values:
             present_evos = test[test["evolves_from_species_id"] == matrix[x:,y][0]].id.values
             matrix[x,y+1] = np.array(list(set(present_evos) & set(all_evos)))[0]
-            all_evos = all_evos[all_evos != matrix[x,y+1]]
+            if len(test[test["evolves_from_species_id"] == present_evos[0]]) <= 1:        
+                all_evos = all_evos[all_evos != matrix[x,y+1]]
             y+=1
 
 matrix = matrix.astype(int)
@@ -138,7 +137,6 @@ evo_matrix = matrix.tolist()
 
 for x in range(len(evo_matrix)):
     evo_matrix[x] = [i for i in evo_matrix[x] if i != 0]
-
 
 
 #Images of the pokemons
@@ -149,97 +147,89 @@ x,y =0,0
 for x in range(nrow_evo):
     y=0
     for a in evo_matrix[x]:
-        evo_image[x].append(Image.open('figures/'+str(int(a))+'.png'))
+        evo_image[x].append(Image.open('figures/pokemon_'+str(int(a))+'.png'))
         y+=1
 
 #Mechanism of evolution
-if len(matrix) > 1:
-     evo_mech = [[] for _ in range(nrow_evo)]
-     for x in range(nrow_evo):
+if ncol_evo > 1:
+    evo_mech = [[] for _ in range(nrow_evo)]
+    for x in range(nrow_evo):
         y=0
         for a in evo_matrix[x][:(len(evo_matrix[0])-1)]:
             evo_mech[x].append(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == matrix[x,y+1]].evolution_trigger_id.values[0])
             y+=1
 
-
-evo_text = [0]*(len(evo_mech))
-evo_object = [0]*(len(evo_mech))
-
-for y in range(len(evo_mech)):
-    if evo_mech[y] == 1:
-        evo_text[y] = 'Nivel '+ str(int(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == matrix[y+1]].minimum_level.values[0]))
-        response = requests.get("https://s2.coinmarketcap.com/static/img/coins/200x200/9696.png")
-        evo_object[y] = Image.open(BytesIO(response.content))
-        newsize=(44,44)
-        evo_object[y]=evo_object[y].resize(newsize)
-    elif evo_mech[y] == 2:
-        evo_text[y] = 'Intercam.'
-        if pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == matrix[1]].held_item_id.isna().values[0]:
-            response = requests.get('https://cdn-icons-png.flaticon.com/512/103/103588.png')
-            evo_object[y] = Image.open(BytesIO(response.content))
-            newsize=(44,44)
-            evo_object[y]=evo_object[y].resize(newsize)
-        else:
-            item = pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == matrix[1]].held_item_id
-            response = requests.get('https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png')
-            evo_object[y] = Image.open(BytesIO(response.content))
-            newsize=(44,44)
-            evo_object[y]=evo_object[y].resize(newsize)
-    elif evo_mech[y] == 3:
-        evo_text[y] = 'Objeto'
-        response = requests.get('https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png')
-        evo_object[y] = Image.open(BytesIO(response.content))
-        newsize=(44,44)
-        evo_object[y]=evo_object[y].resize(newsize)
-    elif evo_mech[y] == 4:
-        evo_text[y] = 'Shedinja'
-        response = requests.get('https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png')
-        evo_object[y] = Image.open(BytesIO(response.content))
-        newsize=(44,44)
-        evo_object[y]=evo_object[y].resize(newsize)
-    elif evo_mech[y] == 5:
-        evo_text[y] = 'Sirfetchd'
-        response = requests.get('https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png')
-        evo_object[y] = Image.open(BytesIO(response.content))
-        newsize=(44,44)
-        evo_object[y]=evo_object[y].resize(newsize)
+if ncol_evo > 1:
+    evo_text = [[] for _ in range(nrow_evo)]
+    evo_object = [[] for _ in range(nrow_evo)]
+    for x in range(nrow_evo):
+        y=0
+        for a in evo_mech[x]:
+            if a == 1:
+                if np.isfinite(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].minimum_level.values[0]):
+                    evo_text[x].append('Nivel '+ str(int(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].minimum_level.values[0])))
+                    evo_object[x].append(ImageLocal("resources/items/item_50.png", 44,44))
+                elif np.isfinite(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].minimum_happiness.values[0]):
+                    evo_text[x].append('Amistad')
+                    evo_object[x].append(ImageLocal("resources/items/item_50.png", 44,44))
+                elif np.isfinite(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].location_id.values[0]):
+                    evo_text[x].append('Ubicación')
+                    evo_object[x].append(ImageLocal("resources/items/item_50.png", 44,44))                    
+                y+=1
+            elif a == 2:
+                evo_text[x].append('Intercamb.')
+                if pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].held_item_id.isna().values[0]:
+                    evo_object[x].append('https://cdn-icons-png.flaticon.com/512/103/103588.png')
+                else:
+                    evo_item=str(int(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].held_item_id.values[0]))
+                    evo_object[x].append(ImageLocal('resources/items/item_'+ evo_item +'.png', 44,44))
+                y+=1
+            elif a == 3:
+                evo_text[x].append('Objeto')
+                evo_item=str(int(pokemon_evolutions[pokemon_evolutions["evolved_species_id"] == evo_matrix[x][y+1]].trigger_item_id.values[0]))
+                evo_object[x].append(ImageLocal('resources/items/item_'+ evo_item +'.png', 44,44))
+                y+=1
+            elif a == 4:
+                evo_text[y] = 'Shedinja'
+                response = requests.get('https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png')
+                evo_object[y] = Image.open(BytesIO(response.content))
+                newsize=(44,44)
+                evo_object[y]=evo_object[y].resize(newsize)
+            elif a == 5:
+                evo_text[y] = 'Sirfetchd'
+                response = requests.get('https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png')
+                evo_object[y] = Image.open(BytesIO(response.content))
+                newsize=(44,44)
+                evo_object[y]=evo_object[y].resize(newsize)
 
 
 #Color of the line
 evo_type = pokemon_types[(pokemon_types["slot"] == 1) & (pokemon_types["pokemon_id"] == evo_matrix[0][0])].type_id.values[0]
 
 with Image.new('RGBA', ((550*len(evo_matrix[0]))-150, (580*len(evo_matrix)))) as im:
-    im.paste(evo_image[0][0], (0, 0))
-    if len(evo_matrix[0]) > 1:
-        im.paste(evo_image[0][1], (550, 0))
-        draw = ImageDraw.Draw(im)
-        draw.rectangle((400, 310, 510, 340), fill=types[evo_type]['color_light'])
-        draw.polygon([(550,325), (510, 290), (510,360)], fill = types[evo_type]['color_light'])
-        draw.text((425,317), evo_text[0], fill = "black", font = font_desc)
-        im.paste(evo_object[0], (444,255))
-        if len(evo_matrix[0]) > 2:
-            im.paste(evo_image[0][2], (1100, 0))
-            draw.rectangle((950, 310, 1060, 340), fill=types[evo_type]['color_light'])
-            draw.polygon([(1100,325), (1060, 290), (1060,360)], fill = types[evo_type]['color_light'])
-            draw.text((975,317), evo_text[1], fill = "black", font = font_desc)
-            im.paste(evo_object[1], (994,255))
-    im.paste(evo_image[1][0], (0, 0+580))
-    if len(evo_matrix[1]) > 1:
-        im.paste(evo_image[1][1], (550, 580))
-        draw = ImageDraw.Draw(im)
-        draw.rectangle((400, 890, 510, 920), fill=types[evo_type]['color_light'])
-        draw.polygon([(550,325+580), (510, 290+580), (510,360+580)], fill = types[evo_type]['color_light'])
-        draw.text((425,317+580), evo_text[0], fill = "black", font = font_desc)
-        im.paste(evo_object[0], (444,255+580))
-        if len(evo_matrix[0]) > 2:
-            im.paste(evo_image[1][2], (1100, 0+580))
-            draw.rectangle((950, 310+580, 1060, 340+580), fill=types[evo_type]['color_light'])
-            draw.polygon([(1100,325+580), (1060, 290+580), (1060,360+580)], fill = types[evo_type]['color_light'])
-            draw.text((975,317+580), evo_text[1], fill = "black", font = font_desc)
-            im.paste(evo_object[1], (994,255+580))
-
-
-all_same([item[1] for item in list]) #for checking if elements are the same ones in a given position for all lists
+    draw = ImageDraw.Draw(im)
+    for x in range(len(evo_matrix)):
+        y = 0
+        for a in evo_matrix[x]:
+            if all_same([item[y] for item in evo_matrix]):
+                im.paste(evo_image[x][y], (0+(550*y), int(580*len(evo_matrix)/2-290)))
+            else:
+                im.paste(evo_image[x][y], (0+(550*y), 0+(580*x)))
+            y+=1
+    for x in range(len(evo_text)):
+        y = 0
+        for text in evo_text[x]:
+            if all_same([item[y+1] for item in evo_matrix]):
+                draw.rectangle((400+(550*y), 310+int(580*len(evo_matrix)/2-290), 510+(550*y), 340+int(580*len(evo_matrix)/2-290)), fill=types[evo_type]['color_light'])
+                draw.polygon([(550+(550*y),325+int(580*len(evo_matrix)/2-290)), (510+(550*y), 290+int(580*len(evo_matrix)/2-290)), (510+(550*y),360+int(580*len(evo_matrix)/2-290))], fill = types[evo_type]['color_light'])
+                draw.text((425+(550*y),317+int(580*len(evo_matrix)/2-290)), text, fill = "black", font = font_desc)
+                im.paste(evo_object[x][y], (444+(550*y),255+int(580*len(evo_matrix)/2-290)))
+            else:
+                draw.rectangle((400+(550*y), 310+(580*x), 510+(550*y), 340+(580*x)), fill=types[evo_type]['color_light'])
+                draw.polygon([(550+(550*y),325+(580*x)), (510+(550*y), 290+(580*x)), (510+(550*y),360+(580*x))], fill = types[evo_type]['color_light'])
+                draw.text((425+(550*y),317+(580*x)), text, fill = "black", font = font_desc)
+                im.paste(evo_object[x][y], (444+(550*y),255+(580*x)))
+            y+=1
 
 
 im.save("out.png")
